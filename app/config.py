814c44be8056945d5f042f29a221a0d1328dad46
Khalid Hesham <khalid-hesham@hotmail.com>
2021-10-24 01:33:42 +0200

    added database migrations

--- app/config.py
+++ app/config.py
@@ -8,6 +8,6 @@ class Config(object):
     DB_USER = os.getenv('POSTGRES_USER',  'postgres')
     DB_PASSWORD = os.getenv('POSTGRES_PASSWORD', '123456789')
     DB_NAME = os.getenv('POSTGRES_DB', 'capstone_dev')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') #'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)) 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
